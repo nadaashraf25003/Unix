db3b979b492176e1506abac59417bd7cebdd481c
nada <nadanadaashraf25@gmail.com>
2026-02-09 18:35:46 +0200

    Update4

--- src/Views/CampusNavigation/components/RoomSelector.jsx
+++ src/Views/CampusNavigation/components/RoomSelector.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import useCampus from "@/hooks/useCampus";
+import useCampus from "@/Hooks/useCampus";
 import { FaDoorClosed, FaChevronDown, FaSpinner } from "react-icons/fa6";
 const RoomSelector = ({ buildingId, onSelect, placeholder = "Select Room..." }) => {
   const [isOpen, setIsOpen] = useState(false);
